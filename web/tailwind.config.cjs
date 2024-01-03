71f1a78b30310afc7d2e7ecccb60c74b14aaa2e4
Gabriel Soler <gabriel.edsoler@gmail.com>
2024-01-03 16:23:36 +0000

    template projeto full stack typescript + node + sqlite

---
+++ web/tailwind.config.cjs
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    // Dentro da pasta 'src', qualquer pasta que tenha qualquer arquivo com a extensão .tsx
+		'./src/**/*.tsx',
+		'./index.html',
+		// Esses 2 caminhos é para dizer onde poderei utilizar o tailwind
+  ],
+  theme: {
+    extend: {
+      colors: {
+				background: '#09090A',
+			},
+      gridTemplateRows: {
+        7: 'repeat(7, minmax(0,1fr))',
+      },
+    },
+  },
+  plugins: [],
+}
+
